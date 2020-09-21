0681f35a1e2cb8da7bf5b9dbbd9ad75071693f38
Fermin <dffermin@ibm.com>
2020-09-21 16:41:48 -0300

    -Creating an endpoint for the root of our API -Creating an endpoint for the highlighted snippets -Hyperlinking our API -Making sure our URL patterns are named -Adding pagination

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 
@@ -15,6 +20,13 @@ class SnippetList(generics.ListCreateAPIView):
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    # Right now, if we created a code snippet, there'd be no way of associating the user that created the snippet,
+    # with the snippet instance. The user isn't sent as part of the serialized representation,
+    # but is instead a property of the incoming request.
+    #
+    # The way we deal with that is by overriding a .perform_create() method on our snippet views,
+    # that allows us to modify how the instance save is managed, and handle any information
+    # that is implicit in the incoming request or requested URL.
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
@@ -22,7 +34,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
@@ -33,3 +45,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
