d282ff29041f13b5f726e033b31be1994f69c44a
Fermin <dffermin@ibm.com>
2020-09-21 14:21:09 -0300

    -adding endpoints for our User models -Associating snippets with Users -Updating our serializer -Adding required permissions to views -Ading login to the browseable API

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import include
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -6,6 +7,12 @@ from snippets import views
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
 
 
 # Using the mixin classes we've rewritten the views to use slightly less code than before,
@@ -10,8 +13,23 @@ from snippets.serializers import SnippetSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
