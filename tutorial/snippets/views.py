834008b181070e164be18dbb1063305c0ae9b1e2
Fermin <dffermin@ibm.com>
2020-09-21 12:54:05 -0300

    Change from function base view to class-based view

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,32 +1,25 @@
+from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-# REST framework provides two wrappers you can use to write API views.
-#
-# The @api_view decorator for working with function based views.
-# The APIView class for working with class-based views.
-# These wrappers provide a few bits of functionality such as making sure you receive Request
-# instances in your view, and adding context to Response objects so that content negotiation can be performed.
-#
-# The wrappers also provide behaviour such as returning 405 Method Not Allowed responses when appropriate,
-# and handling any ParseError exceptions that occur when accessing request.data with malformed input.
+# We can also write our API views using class-based views, rather than function based views.
+# As we'll see this is a powerful pattern that allows us to reuse common functionality, and helps us keep our code DRY.
+class SnippetList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -34,27 +27,31 @@ def snippet_list(request, format=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+class SnippetDetail(APIView):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve, update or delete a snippet instance.
     """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
